9563a31b435dbef8726b851f28a3959b3ddc143b
Your NameZachGaskin <zgaskin@ufclab.com>
2019-02-08 17:06:46 -0500

    work html to react

--- src/components/SignUpForm/SupForm.js
+++ src/components/SignUpForm/SupForm.js
@@ -1,30 +1,33 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
-import { Helmet } from 'react-helmet'
+import { Section } from '../../utils'
 
-export default class SupForm extends Component {
+export class SupForm extends Component {
+  rawMarkup() {
+    var rawMarkup = this.props.content
+    return { __html: rawMarkupc }
+  }
   render() {
     return (
-      <FormWrapper>
-        <div
-          id="mf_placeholder"
-          data-formurl="https://littleeaglefootball.forms-db.com/embed.php?id=11477"
-          data-formheight="6315"
-          data-formtitle="2019 Football & Cheer Registration Copy"
-          data-paddingbottom="10"
-        />
-        <script>
-          {(function(f, o, r, m) {
-            r = f.createElement('script')
-            r.async = 1
-            r.src = o + 'js/mf.js'
-            m = f.getElementById('mf_placeholder')
-            m.parentNode.insertBefore(r, m)
-          })(document, 'https://littleeaglefootball.forms-db.com/')}
-        </script>
-      </FormWrapper>
+      <Section>
+        <div className="modal-body">
+          <span dangerouslySetInnerHTML={this.rawMarkup()} />
+        </div>
+      </Section>
     )
   }
 }
 
-const FormWrapper = styled.div``
+const rawMarkupc = `
+<div id="mf_placeholder" 
+     data-formurl="https://littleeaglefootball.forms-db.com/embed.php?id=11477" 
+     data-formheight="6315"  
+     data-formtitle="2019 Football & Cheer Registration Copy"  
+     data-paddingbottom="10">
+</div>
+<script>
+	(function(f,o,r,m){
+		r=f.createElement('script');r.async=1;r.src=o+'js/mf.js';
+		m=f.getElementById('mf_placeholder'); m.parentNode.insertBefore(r, m);
+	})(document,'https://littleeaglefootball.forms-db.com/');
+</script>
+`

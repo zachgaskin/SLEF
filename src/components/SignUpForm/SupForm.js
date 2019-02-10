61a8cd38fc93721d25bd0d6880abd32305b7e916
Zach Gaskin <zgaskin@ufclab.com>
2019-02-09 22:19:37 -0500

    machform working

--- src/components/SignUpForm/SupForm.js
+++ src/components/SignUpForm/SupForm.js
@@ -1,44 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Section } from '../../utils'
+import htmlContent from '../SignUpForm/machform.html'
+import createReactClass from 'create-react-class'
 
-<<<<<<< HEAD
-export default class QuickInfo extends Component {
+const MyComponent = createReactClass({
   render() {
     return (
       <Section>
-        dangerouslySetInnerHTML={{ __html: `<div> '/machform.html' </div>` }}
-=======
-export class SupForm extends Component {
-  rawMarkup() {
-    var rawMarkup = this.props.content
-    return { __html: rawMarkupc }
-  }
-  render() {
-    return (
-      <Section>
-        <div className="modal-body">
-          <span dangerouslySetInnerHTML={this.rawMarkup()} />
-        </div>
->>>>>>> 9563a31b435dbef8726b851f28a3959b3ddc143b
+        <iframe>
+          <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+        </iframe>
       </Section>
     )
-  }
-}
-<<<<<<< HEAD
-=======
-
-const rawMarkupc = `
-<div id="mf_placeholder" 
-     data-formurl="https://littleeaglefootball.forms-db.com/embed.php?id=11477" 
-     data-formheight="6315"  
-     data-formtitle="2019 Football & Cheer Registration Copy"  
-     data-paddingbottom="10">
-</div>
-<script>
-	(function(f,o,r,m){
-		r=f.createElement('script');r.async=1;r.src=o+'js/mf.js';
-		m=f.getElementById('mf_placeholder'); m.parentNode.insertBefore(r, m);
-	})(document,'https://littleeaglefootball.forms-db.com/');
-</script>
-`
->>>>>>> 9563a31b435dbef8726b851f28a3959b3ddc143b
+  },
+})
+export default MyComponent
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Supform from '../components/SignUpForm/SupForm'
+import MachForm from '../components/SignUpForm/MForm'
 
 const SignupPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <Supform />
+    <MachForm />
   </Layout>
 )
 

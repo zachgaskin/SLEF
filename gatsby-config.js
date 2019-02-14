85a7acd883dad14076dced4e17719d6b003a1414
Your NameZachGaskin <zgaskin@ufclab.com>
2019-02-14 15:28:31 -0500

    config image issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/SLEF-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
@@ -39,42 +39,10 @@ module.exports = {
         plugins: [
           `gatsby-remark-responsive-iframe`,
           `gatsby-remark-wrap-iframe`,
-          {
-            resolve: `gatsby-plugin-favicon`,
-            options: {
-              logo: './src/images/SLEF-icon.png',
-
-              // WebApp Manifest Configuration
-              appName: null, // Inferred with your package.json
-              appDescription: null,
-              developerName: null,
-              developerURL: null,
-              dir: 'auto',
-              lang: 'en-US',
-              background: '#fff',
-              theme_color: '#fff',
-              display: 'standalone',
-              orientation: 'any',
-              start_url: '/?homescreen=1',
-              version: '1.0',
-
-              icons: {
-                android: true,
-                appleIcon: true,
-                appleStartup: true,
-                coast: false,
-                favicons: true,
-                firefox: true,
-                opengraph: false,
-                twitter: false,
-                yandex: false,
-                windows: false,
-              },
-            },
-          },
         ],
       },
     },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',

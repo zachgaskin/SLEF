b8e4c075d6df3f0f5982a14450b28859cdcb6ff2
Your NameZachGaskin <zgaskin@ufclab.com>
2019-02-14 15:11:51 -0500

    favicon and fixed index signup button

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,16 @@ import SEO from '../components/seo'
 import { HomeHeader, Banner, BannerButton } from '../utils'
 import img from '../images/bcg/mightymite.jpg'
 import QuickInfo from '../components/HomePageComponents/QuickInfo'
+import { Link } from 'gatsby'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <HomeHeader img={img}>
       <Banner title="Somerset" subtitle="Youth Football & Cheer">
-        <BannerButton style={{ margin: '2rem auto' }}>Sign Up</BannerButton>
+        <Link to="/signup/" style={{ textDecoration: 'none' }}>
+          <BannerButton style={{ margin: '2rem auto' }}>Sign Up</BannerButton>
+        </Link>
       </Banner>
     </HomeHeader>
     <QuickInfo />

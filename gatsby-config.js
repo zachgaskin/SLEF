80b1f85c03535002f4ce5d24b9d04ce7472227ed
Your NameZachGaskin <zgaskin@ufclab.com>
2019-02-26 17:06:32 -0500

    cms config added more to filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {

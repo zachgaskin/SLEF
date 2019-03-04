b1a1bd56baf0fbcf889b4db19955152152e670de
Zach Gaskin <zgaskin@ufclab.com>
2019-03-03 20:14:46 -0500

    about-page mods

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -2,11 +2,16 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import { Section } from '../utils'
+import aboutImg from '../images/bcg/undercenter.jpg'
+import { PageHeader, Banner } from '../utils'
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
+      <PageHeader img={aboutImg}>
+        <Banner title="About Us" subtitle="Youth Football & Cheer" />
+      </PageHeader>
       <Section>
         <div>
           <h1>{post.frontmatter.title}</h1>

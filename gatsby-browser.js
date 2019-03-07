003f055c67a551716ea6f36c4f3550ccd81b7b92
Zach Gaskin <zgaskin@ufclab.com>
2019-03-07 08:01:43 -0500

    intersection observer fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,19 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+exports.onClientEntry = () => {
+  // IntersectionObserver polyfill for gatsby-image (Safari, IE)
+  if (typeof window.IntersectionObserver === `undefined`) {
+    import(`intersection-observer`)
+    console.log(`👍 IntersectionObserver is polyfilled`)
+  }
 
-// You can delete this file if you're not using it
+  // Object-fit/Object-position polyfill for gatsby-image (IE)
+  const testImg = document.createElement(`img`)
+  if (
+    typeof testImg.style.objectFit === `undefined` ||
+    typeof testImg.style.objectPosition === `undefined`
+  ) {
+    import('object-fit-images').then(({ default: ObjectFitImages }) =>
+      ObjectFitImages()
+    )
+    console.log(`👍 Object-fit/Object-position are polyfilled`)
+  }
+}

89d013246475bacfe1e378d0c629440a0d1c47c7
Zach Gaskin <zgaskin@ufclab.com>
2019-03-08 08:00:50 -0500

    gbrowser mod for polyfillio

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,19 +1,32 @@
 exports.onClientEntry = () => {
-  // IntersectionObserver polyfill for gatsby-image (Safari, IE)
-  if (typeof window.IntersectionObserver === `undefined`) {
-    import(`intersection-observer`)
-    console.log(`👍 IntersectionObserver is polyfilled`)
-  }
+  return new Promise((resolve, reject) => {
+    window.__polyfillio__ = () => {
+      resolve()
+    }
 
-  // Object-fit/Object-position polyfill for gatsby-image (IE)
-  const testImg = document.createElement(`img`)
-  if (
-    typeof testImg.style.objectFit === `undefined` ||
-    typeof testImg.style.objectPosition === `undefined`
-  ) {
-    import('object-fit-images').then(({ default: ObjectFitImages }) =>
-      ObjectFitImages()
-    )
-    console.log(`👍 Object-fit/Object-position are polyfilled`)
-  }
+    const features = []
+
+    if (!('Intl' in window)) {
+      const locale = window.location.pathname.split('/')[1]
+      features.push(`Intl.~locale.${locale}`)
+    }
+
+    if (!('fetch' in window)) {
+      features.push('fetch')
+    }
+
+    // ... detect other missing features
+
+    if (features.length) {
+      const s = document.createElement('script')
+      s.src = `https://cdn.polyfill.io/v2/polyfill.min.js?features=${features.join(
+        ','
+      )}&rum=1&flags=always&callback=__polyfillio__`
+      s.async = true
+      s.onerror = reject
+      document.head.appendChild(s)
+    } else {
+      resolve()
+    }
+  })
 }

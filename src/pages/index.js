4a73abcc76f56b960f448e5320a7c2c41e630f28
Zach Gaskin <zgaskin@ufclab.com>
2019-03-10 19:33:58 -0400

    next

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,9 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import StyledBackgroundSection from '../components/bgImage'
-import { HomeHeader, Banner, BannerButton } from '../utils'
-import img from '../images/bcg/mightymite2.jpg'
+
 import QuickInfo from '../components/HomePageComponents/QuickInfo'
 import Gallery from '../components/HomePageComponents/Gallery'
-import { Link } from 'gatsby'
 
 const IndexPage = () => (
   <Layout>

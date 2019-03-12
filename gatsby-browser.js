797e12332fdd11534bfa174a3a909bc81dd044f7
Zach Gaskin <zgaskin@ufclab.com>
2019-03-12 07:45:59 -0400

    IO2

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,18 +1,17 @@
 export const onClientEntry = async () => {
-    if (typeof IntersectionObserver === `undefined`) {
-      await import(`intersection-observer`);
-    }
+  if (typeof IntersectionObserver === `undefined`) {
+    await import(`intersection-observer`)
   }
-  
-  // Object-fit/Object-position polyfill for gatsby-image (IE)
-  const testImg = document.createElement(`BackgroundImage`)
-  if (
-    typeof testImg.style.objectFit === `undefined` ||
-    typeof testImg.style.objectPosition === `undefined`
-  ) {
-    import('object-fit-images').then(({ default: ObjectFitImages }) =>
-      ObjectFitImages()
-    )
-    console.log(`👍 Object-fit/Object-position are polyfilled`)
-  }
+}
+
+// Object-fit/Object-position polyfill for gatsby-image (IE)
+const testImg = document.createElement(`BackgroundImage`)
+if (
+  typeof testImg.style.objectFit === `undefined` ||
+  typeof testImg.style.objectPosition === `undefined`
+) {
+  import('object-fit-images').then(({ default: ObjectFitImages }) =>
+    ObjectFitImages()
+  )
+  console.log(`👍 Object-fit/Object-position are polyfilled`)
 }

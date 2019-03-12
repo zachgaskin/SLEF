7aa48bba73ba01aa293ec7759b34ecbb98b8f192
Your NameZachGaskin <zgaskin@ufclab.com>
2019-03-12 10:19:39 -0400

    test remove other children from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,15 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import StyledBackgroundSection from '../components/bgImage'
 
-import QuickInfo from '../components/HomePageComponents/QuickInfo'
-import Gallery from '../components/HomePageComponents/Gallery'
+//import QuickInfo from '../components/HomePageComponents/QuickInfo'
+//import Gallery from '../components/HomePageComponents/Gallery'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <StyledBackgroundSection />
-    <QuickInfo />
-    <Gallery />
+    {/*<QuickInfo /> 
+    <Gallery />*/}
   </Layout>
 )
 

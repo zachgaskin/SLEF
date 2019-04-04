4fe8f1f6c1b2bff68622716315706ff5fca7181a
Your NameZachGaskin <zgaskin@ufclab.com>
2019-04-04 08:59:02 -0400

    contact-page template

---
+++ src/templates/contact-page.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import { Section, Hero, Banner } from '../utils'
+import ContactBGI from '../components/ContactBGI'
+
+export default ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <Hero>
+        <ContactBGI />
+        <Banner title="CONTACT US" subtitle="Youth Football & Cheer" />
+      </Hero>
+      <Section>
+        <div>
+          <hi>Hello Contact Us</hi>
+          <h1>{post.frontmatter.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        </div>
+      </Section>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
